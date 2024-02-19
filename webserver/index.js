ba640369244e3d15495b7eba1ebf6c3bc2b7afbf
wivwiv <bigpar@qq.com>
2024-02-19 10:43:49 +0800

    Update EMQX version and README

--- webserver/index.js
+++ webserver/index.js
@@ -6,9 +6,11 @@ const dbPath = path.join(__dirname, 'db.json');
 if (!fs.existsSync(dbPath)) {
   fs.writeFileSync(dbPath, JSON.stringify({
     devices: [],
+    eventsHistory: [],
     messages: [],
   }))
 }
+
 const presetToken = 'B53498D3-1752-4AA7-BACA-7013309B7468';
 
 const eventHandlers = {
@@ -16,7 +18,7 @@ const eventHandlers = {
     console.log(data);
     return { code: 0, message: 'handle by log' }
   },
-  'message.publish': (data) => {
+  'message.publish': (data, db) => {
     const msg = {
       topic: data.topic,
       payload: data.payload,
@@ -24,14 +26,30 @@ const eventHandlers = {
       clientId: data.clientid,
       createdAt: new Date().toISOString(),
     }
-    const db = require(dbPath)
     db.messages.push(msg)
-    fs.writeFileSync(dbPath, JSON.stringify(db, null, 2))
     return msg;
   },
-  connectedAndDisconnected: (data) => {
+  eventsHistory: (data, db) => {
+    db.eventsHistory.push({
+      event: data.event,
+      clientId: data.clientid,
+      username: data.username,
+      peername: data.peername,
+      options: {
+        proto_ver: data.proto_ver,
+        keepalive: data.keepalive,
+        clean_start: data.clean_start,
+        reason: data.reason,
+        topic: data.topic,
+        qos: data.qos,
+        node: data.node,
+      },
+      createdAt: new Date().toISOString(),
+    })
+  },
+  connectedAndDisconnected: (data, db) => {
     const connected = data.event === 'client.connected'
-    const db = require(dbPath)
+    eventHandlers.eventsHistory(data, db)
     let device = db.devices.find($ => $.clientId === data.clientid)
     if (!device) {
       device = {
@@ -52,12 +70,12 @@ const eventHandlers = {
         connectedAt: new Date().toISOString(),
       })
     }
-    fs.writeFileSync(dbPath, JSON.stringify(db, null, 2))
     return device;
   }
 }
 
 const server = http.createServer((req, res) => {
+  let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'))
   if (req.method === 'POST') {
     // Auth
     const authorizationHeader = req.headers['authorization'];
@@ -84,12 +102,15 @@ const server = http.createServer((req, res) => {
         const data = JSON.parse(body);
         if (['client.disconnected', 'client.connected'].includes(eventname)) {
           eventname = 'connectedAndDisconnected'
+        } else if (['session.subscribed', 'session.unsubscribed'].includes(eventname)) {
+          eventname = 'eventsHistory'
         }
         const eventHandler = eventHandlers[eventname] || eventHandlers.log
         console.log(req.method, req.url)
         if (eventHandler) {
           res.writeHead(200, { 'Content-Type': 'application/json' });
-          const result = eventHandlers[eventname](data);
+          const result = eventHandlers[eventname](data, db);
+          fs.writeFileSync(dbPath, JSON.stringify(db, null, 2))
           res.end(JSON.stringify(result));
         } else {
           res.writeHead(404, { 'Content-Type': 'application/json' });
@@ -109,13 +130,11 @@ const server = http.createServer((req, res) => {
   } else if (req.method === 'GET') {
     // GET /messages
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    const db = require(dbPath)
-
     res.end(JSON.stringify({
-      devocesCount: db.devices.length,
-      messagesCount: db.messages.length,
-      devices: db.devices,
-      messages: db.messages,
+      deviceCount: db.devices.length,
+      messageCount: db.messages.length,
+      eventsHistoryCount: db.eventsHistory.length,
+      ...db,
     }));
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
